f27d438bd6f2dc2ac9d33bae6cbfa3e7c580ec38
Renzo Belvedere <renzobelve@MacBook-Pro-de-Renzo.local>
2016-09-27 18:03:08 -0300

    Capa de Persistencia

--- src/main/java/loader/PlayerLoader.java
+++ src/main/java/loader/PlayerLoader.java
@@ -1,16 +1,10 @@
 package loader;
 
-import exception.GameEmptyException;
-import exception.GameFullException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import model.Game;
 import model.Player;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
-import repository.GameRepository;
 import repository.PlayerRepository;
 
 /**
@@ -24,9 +18,6 @@ public class PlayerLoader implements ApplicationListener<ContextRefreshedEvent>{
     
     @Autowired
     private PlayerRepository playerRepository;
-    
-    @Autowired
-    private GameRepository gameRepository;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent e) {
